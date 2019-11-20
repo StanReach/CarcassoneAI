fae149c1a3bf3fc4920f268695c4002a2befc06e
Stanley Reach <stanreach@gmail.com>
2019-11-20 22:28:46 +0100

    building reading of tile logic

---
+++ src/AILogic.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AILogic {
+
+    Scanner userInput;
+
+    public AILogic() {
+        userInput = new Scanner(System.in);
+    }
+
+    boolean getDescriptionInputOfPiece() {
+        return false;
+    }
+
+    identifyPiece() {
+
+    }
+}
